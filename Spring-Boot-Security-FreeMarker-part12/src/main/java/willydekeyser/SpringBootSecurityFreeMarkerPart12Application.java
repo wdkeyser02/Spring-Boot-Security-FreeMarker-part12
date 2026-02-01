4adc3489501f6bc3dca226ef86a95e94dfeec3bd
Willy De Keyser <spring.boot.programming@gmail.com>
2026-02-01 14:07:08 +0100

    Init

---
+++ Spring-Boot-Security-FreeMarker-part12/src/main/java/willydekeyser/SpringBootSecurityFreeMarkerPart12Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityFreeMarkerPart12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityFreeMarkerPart12Application.class, args);
+	}
+
+}
---
+++ Spring-Boot-Security-FreeMarker-part12/src/test/java/willydekeyser/SpringBootSecurityFreeMarkerPart12ApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityFreeMarkerPart12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
